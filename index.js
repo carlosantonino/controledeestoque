cfb66d9b3f95e73d4a7402d5aa02c47d41b79572
Carlos Antonino <carlosantonino.cs@gmail.com>
2021-08-28 17:14:25 -0300

    criando index para teste de rotas

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res) {
+    res.send("Gerenciador Financeiro");
+});
+
+app.get("/contato", function(req, res) {
+    res.send("Pagina de Contato");
+});
+
+app.listen(8080);
+
